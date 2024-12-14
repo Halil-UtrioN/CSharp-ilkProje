5b73366a109fbf40a159f1f4f550a7e9663533b9
Halil İbrahim ÇAKIR <hibrahim54@cakirhalil.com>
2024-12-14 14:10:48 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// Değişkenler BÜYÜK-küçük harf duyarlıdır.
+/*
+    Burası çok satırlı yorum bloğu
+*/
+
+var sayi = 20; //Tam sayı değişken (Int)
+var urunAdi = "Samsung A35 5G 256GB"; //String değer
+var ondalikSayi = 484.25; // Ondalık sayı değişken (double)
+var urunStatus = true; // true-false değişkeni (boolean)
+
+Console.WriteLine(sayi);
+Console.WriteLine(urunAdi);
+Console.WriteLine(ondalikSayi);
+Console.WriteLine(urunStatus);
+Console.WriteLine("________________________________________");
+
+var ogranciAdi = "Halil İbrahim";
+var ogranciSoyadi = "ÇAKIR";
+var ogrenciAdiVeSoyadi = ogranciAdi + " " + ogranciSoyadi;
+var ogrenciNo = "484";
+var ogrenciTc = "11111111111";
+var ogrenciDogumYili = 2007;
+var ogranciYas = 2024 - ogrenciDogumYili;
+var ogranciAdres = "SAKARYA";
+
+Console.WriteLine(ogranciAdi);
+Console.WriteLine(ogranciSoyadi);
+Console.WriteLine(ogrenciAdiVeSoyadi);
+Console.WriteLine(ogrenciNo);
+Console.WriteLine(ogrenciTc);
+Console.WriteLine(ogrenciDogumYili);
+Console.WriteLine(ogranciYas);
+Console.WriteLine(ogranciAdres);
